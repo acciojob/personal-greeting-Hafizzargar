2bc7c58fc34e4488476695dd08afbfac656f7eb6
Hafizzargar <hafezzargar987@gmail.com>
2024-08-16 14:32:52 +0530

    code add

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,22 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [txt,settxt]=useState("");
+  function enter(e){
+    return settxt(e.target.value);
+
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+
+        <input type="text" placeholder="Enter input" value="{txt}" onchange="enter"/>
+        <div>{txt}</div>
+       
+
+
+
+
     </div>
   )
 }
