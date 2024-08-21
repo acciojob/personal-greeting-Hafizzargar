f78cb6628480430e15f63076fe329388d08c6502
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2024-08-21 17:50:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,21 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-import { useState } from "react";
 
 const App = () => {
-   const [name, setName] = useState("");
-    return(
-            <div>
-                  <label htmlFor="nam">Enter your name:</label>
-                 // <input value={name} type="text" onChange={(e)=>setName(e.target.value)} onMouseLeave={(e)=>setName("")}/>
-                  <input type="text" onChange={(e) => setName(e.target.value)} value={name} id="nam"/>      
+  const [text, setText] = useState("");
 
-                  
-                     <p>{text && text.length>0?"Hello "+ text+"!":""}</p>
-                  
-                
-            </div>
-        );
+  return (
+    <div>
+      <label htmlFor="name">Enter your name:</label>
+      <input 
+        type="text" 
+        id="name" 
+        onChange={e => setText(e.target.value)} 
+      />
+     <p>{text && text.length>0?"Hello "+ text+"!":""}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
