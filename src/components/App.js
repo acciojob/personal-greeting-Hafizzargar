0a9887a392344227b7012253f9fc271159326567
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2024-08-21 16:33:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,13 @@ const App = () => {
    let[name, setName] = useState("");
     return(
             <div>
-                <p>Enter your name:</p> 
-                <input type="text" onChange={(e)=>setName("Hello " + e.target.value +"!")}/>
-                <p>{name}</p>
+                 <p>Enter your name:</p> 
+                <input type="text" onChange={(e)=>setName(e.target.value)}/>
+                <p>
+                  {
+                    name==false?null:<p>Hello {name}!</p>
+                  }
+                </p>
             </div>
         );
 }
