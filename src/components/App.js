f719ad43370d4051806196e4478c53cf8646e02f
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2024-08-21 16:38:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const App = () => {
                 <input type="text" onChange={(e)=>setName(e.target.value)}/>
                 
                   {
-                    name.trim()==false?null:<p>Hello {name}!</p>
+                     name.trim() && <p>Hello {name}!</p>
                   }
                 
             </div>
